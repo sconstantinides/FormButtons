e5ca47f118c6eca5741091e723a8e338a59f8e72
Stelios Constantinides <sconstantinides@gmail.com>
2015-08-06 13:56:00 -0500

    fix bug that was making duplicate calls

--- js/form_buttons.js
+++ js/form_buttons.js
@@ -1,23 +1,28 @@
 $(function() {
-  $(".form-button").click(function() {
-    var fullWidth = $(this).hasClass("full-width");
+  $(".form-button").click(function(event) {
+    if(!$(event.target).is("label") &&
+       !$(event.target).parent().is("label") &&
+       !$(this).hasClass("active")) {
 
-    var containerWidth = $(this).outerWidth();
-    if(fullWidth) {
-      containerWidth = $(this).parent().width();
+      var fullWidth = $(this).hasClass("full-width");
+
+      var containerWidth = $(this).outerWidth();
+      if(fullWidth) {
+        containerWidth = $(this).parent().width();
+      }
+
+      var input = $(this).find(".input");
+
+      var width = containerWidth - $(this).find(".icon").outerWidth() - $(this).find(".submit").outerWidth() - (input.outerWidth() - input.width());
+
+      if(fullWidth) {
+        input.animate({ width: width });
+      } else {
+        input.css("width", width);
+      }
+
+      $(this).addClass("active");
+      input.focus();
     }
-
-    var input = $(this).find(".input");
-
-    var width = containerWidth - $(this).find(".icon").outerWidth() - $(this).find(".submit").outerWidth() - (input.outerWidth() - input.width());
-
-    if(fullWidth) {
-      input.animate({ width: width });
-    } else {
-      input.css("width", width);
-    }
-
-    $(this).addClass("active");
-    input.focus();
   });
 });
